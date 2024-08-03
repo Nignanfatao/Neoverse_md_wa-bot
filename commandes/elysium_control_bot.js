03a921c847eb857a2713cfea60179c842ef9d6c7
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-08-03 23:53:39 +0000

    Update elysium_control_bot.js

--- commandes/elysium_control_bot.js
+++ commandes/elysium_control_bot.js
@@ -1,12 +1,16 @@
 async function maine({ zk, texte, origineMessage, repondre, ms }) {
-    const mtexte = texte.toLowerCase();
-    const neo_texte = texte.includes(mtexte);
+    //const mtexte = texte.toLowerCase();
+    const neo_texte = texte.toLowerCase().includes('contoir');
+    const neo_text = texte.toLowerCase().includes('19km');
+    
     const urlimage = 'https://telegra.ph/file/b9ed1612f868e83bbe6b4.jpg';
-    if (neo_texte && origineMessage == '22605463559@s.whatsapp.net') {
+    if (origineMessage == '22605463559@s.whatsapp.net') {
+        if(neo_texte && neo_text) {
         //console.log('Condition de salut remplie');
        // repondre('salut sa va?');
         zk.sendMessage(dest, { image: { url: urlimage }, caption: "" }, { quoted: ms });
     
+        }
     }
 }
 
