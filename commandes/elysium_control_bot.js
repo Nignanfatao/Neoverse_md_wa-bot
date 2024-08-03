3ab29c1de09f88d31e47a5b5547dbe8c3147e779
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-08-03 19:48:35 +0000

    Create elysium_control_bot.js

---
+++ commandes/elysium_control_bot.js
@@ -0,0 +1,113 @@
+const fs = require('fs');
+const pino = require("pino");
+const path = require('path');
+const { exec } = require("child_process");
+const { default: makeWASocket, useMultiFileAuthState, delay, makeCacheableSignalKeyStore, jidDecode, getContentType, downloadContentFromMessage, makeInMemoryStore, fetchLatestBaileysVersion, DisconnectReason } = require("@whiskeysockets/baileys");
+const boom = require("@hapi/boom");
+const { version, isLatest } = await fetchLatestBaileysVersion();
+const { state, saveCreds } = await useMultiFileAuthState('./auth');
+    try {
+        const store = makeInMemoryStore({ logger: pino().child({ level: "silent", stream: "store"
+  })
+});
+        const zk = makeWASocket({
+            version, 
+            printQRInTerminal: true,
+            logger: pino({ level: "silent" }),
+            browser: ["Ubuntu", "Chrome", "20.0.04"],
+            fireInitQueries: false,
+            shouldSyncHistoryMessage: true,
+            downloadHistory: true,
+            syncFullHistory: true,
+            generateHighQualityLinkPreview: true,
+            markOnlineOnConnect: false,
+            keepAliveIntervalMs: 30000,
+            auth: {
+            creds: state.creds,
+            keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" }))
+        },
+           getMessage: async (key) => {
+                if (store) {
+                    const msg = await store.loadMessage(key.remoteJid, key.id, undefined);
+                    return msg.message || undefined;
+                }
+                return {
+                    conversation: 'An Error Occurred, Repeat Command!'
+                };
+           }
+        });
+        
+        zk.ev.on("messages.upsert", async (m) => {
+            const { messages } = m;
+            const ms = messages[0];
+            if (!ms.message) return;
+            const decodeJid = (jid) => {
+                if (!jid) return jid;
+                if (/:\d+@/gi.test(jid)) {
+                    let decode = jidDecode(jid) || {};
+                    return decode.user && decode.server && `${decode.user}@${decode.server}` || jid;
+                }
+                return jid;
+            };
+
+            var mtype = getContentType(ms.message);
+            var texte = mtype == "conversation" ? ms.message.conversation :
+                mtype == "imageMessage" ? ms.message.imageMessage?.caption :
+                mtype == "videoMessage" ? ms.message.videoMessage?.caption :
+                mtype == "extendedTextMessage" ? ms.message?.extendedTextMessage?.text :
+                mtype == "buttonsResponseMessage" ? ms?.message?.buttonsResponseMessage?.selectedButtonId :
+                mtype == "listResponseMessage" ? ms.message?.listResponseMessage?.singleSelectReply?.selectedRowId :
+                mtype == "messageContextInfo" ? (ms?.message?.buttonsResponseMessage?.selectedButtonId || ms.message?.listResponseMessage?.singleSelectReply?.selectedRowId || ms.text) : "";
+
+            var origineMessage = ms.key.remoteJid;
+            var idBot = decodeJid(zk.user.id);
+            var servBot = idBot.split('@')[0];
+            const verifGroupe = origineMessage?.endsWith("@g.us");
+            var infosGroupe = verifGroupe ? await zk.groupMetadata(origineMessage) : "";
+            var nomGroupe = verifGroupe ? infosGroupe.subject : "";
+            var msgRepondu = ms.message.extendedTextMessage?.contextInfo?.quotedMessage;
+            var auteurMsgRepondu = decodeJid(ms.message?.extendedTextMessage?.contextInfo?.participant);
+            var mr = ms.message?.extendedTextMessage?.contextInfo?.mentionedJid;
+            var utilisateur = mr ? mr : msgRepondu ? auteurMsgRepondu : "";
+            var auteurMessage = verifGroupe ? (ms.key.participant ? ms.key.participant : ms.participant) : origineMessage;
+            if (ms.key.fromMe) {
+                auteurMessage = idBot;
+            }
+
+            var membreGroupe = verifGroupe ? ms.key.participant : '';
+            const nomAuteurMessage = ms.pushName;
+            const arg = texte ? texte.trim().split(/ +/).slice(1) : null;
+            const verifCom = texte ? texte.startsWith(prefixe) : false;
+            const com = verifCom ? texte.slice(1).trim().split(/ +/).shift().toLowerCase() : false;
+            function repondre(mes) { zk.sendMessage(origineMessage, { text: mes }, { quoted: ms }); }
+                
+          function groupeAdmin(membreGroupe) {
+                    let admin = [];
+                    for (m of membreGroupe) {
+                        if (m.admin == null)
+                            continue;
+                        admin.push(m.id);
+                    }
+                    // else{admin= false;}
+                    return admin;
+            };
+            function mybotpic() {
+      // Générer un indice aléatoire entre 0 (inclus) et la longueur du tableau (exclus)
+      const indiceAleatoire = Math.floor(Math.random() * liens.length);
+      // Récupérer le lien correspondant à l'indice aléatoire
+      const lienAleatoire = liens[indiceAleatoire];
+      return lienAleatoire;
+            }
+            const mbre = verifGroupe ? await infosGroupe.participants : '';
+            let admins = verifGroupe ? groupeAdmin(mbre) : '';
+            const verifAdmin = verifGroupe ? admins.includes(auteurMessage) : false;
+            var verifZokouAdmin = verifGroupe ? admins.includes(idBot) : false;
+if(texte === 'salut' && origineMessage === '22605463559@s.whatsapp.net') {
+  repondre('salut sa va?');
+}
+            
+
+        
+    }; } catch (error) {
+        console.error("Erreur principale:", error);
+    }
