44df191c4c3546581c8c18355ec6c963ad5fab1b
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-08-03 21:29:21 +0000

    Update elysium_control_bot.js

--- commandes/elysium_control_bot.js
+++ commandes/elysium_control_bot.js
@@ -1,5 +1,5 @@
-async function main({ texte, origineMessage, repondre }) {
-    if (texte === 'salut' && origineMessage === '22605463559@s.whatsapp.net') {
+async function maine({ texte, origineMessage, repondre }) {
+    if (texte == 'salut' && origineMessage == '22605463559@s.whatsapp.net') {
         console.log('Condition de salut remplie');
         repondre('salut sa va?');
     }
