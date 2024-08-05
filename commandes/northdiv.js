9c781a530bfbe4b589fae264ebad99ef1a70be20
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-08-05 09:49:02 +0000

    Update northdiv.js

--- commandes/northdiv.js
+++ commandes/northdiv.js
@@ -1769,6 +1769,12 @@ zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/4fc0cc36b556d4c7ad
   }
 );
 
+const { zokou } = require('../framework/zokou');
+const { getData } = require('../bdd/northdiv');
+const s = require("../set");
+
+const dbUrl = s.DB;
+
 zokou(
   {
     nomCom: 'northlone👤',
@@ -1779,12 +1785,6 @@ zokou(
     let client;
     try {
       const data = await getData('14');
-      let joueur = arg[1];
-      let object = arg[3];
-      let signe = arg[4];
-      let valeur = arg[5];
-      let texte = arg.slice(5).join(' ');
-
       if (!arg || arg.length === 0) {
         let mesg = `.                     *👤𝗜𝗡𝗙𝗢𝗦 𝗗𝗘 𝗝𝗢𝗨𝗘𝗨𝗥𝗦👤*
 ▓ ▓ ▓ ▓▓▓▓▓▓ ▓▓▓ ▓ ▓
@@ -1814,118 +1814,117 @@ ${data.e17}
 ░░░░░░░░░░░░░░░░░░░
 ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
                                        *🔷𝗡Ξ𝗢🔝*`;
-zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/5901ab31b72ad80f1c498.jpg' }, caption: mesg }, { quoted: ms });
-       } else {
+        zk.sendMessage(dest, { image: { url: 'https://telegra.ph/file/5901ab31b72ad80f1c498.jpg' }, caption: mesg }, { quoted: ms });
+      } else {
         if (superUser) { 
-     // const proConfig = {
-                connectionString: dbUrl,
-                ssl: {
-                    rejectUnauthorized: false,
-                },
-            };
+          const proConfig = {
+            connectionString: dbUrl,
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          };
 
-            const { Pool } = require('pg');
-            const pool = new Pool(proConfig);
-            client = await pool.connect();
+          const { Pool } = require('pg');
+          const pool = new Pool(proConfig);
+          client = await pool.connect();
 
-            if (superUser) {
-                let colonnesJoueur = {
-                    pseudo: "e1",
-                    division: "e2",
-                    classe: "e3",
-                    rang: "e4",
-                    golds: "e5",
-                    neocoins: "e6",
-                    gift_box: "e7",
-                    coupons: "e8",
-                    neopass: "e9",
-                    talent: "e10",
-                    victoires: "e12",
-                    defaites: "e13",
-                    trophees: "e14",
-                    tos: "e15",
-                    awards: "e16",
-                    cards: "e17",
-                    globes: "e22", 
-                    pos: "e18"
-                };
+          let colonnesJoueur = {
+            pseudo: "e1",
+            division: "e2",
+            classe: "e3",
+            rang: "e4",
+            golds: "e5",
+            neocoins: "e6",
+            gift_box: "e7",
+            coupons: "e8",
+            neopass: "e9",
+            talent: "e10",
+            victoires: "e12",
+            defaites: "e13",
+            trophees: "e14",
+            tos: "e15",
+            awards: "e16",
+            cards: "e17",
+            globes: "e22", 
+            pos: "e18"
+          };
 
-                let updates = [];
-                let i = 0;
+          let updates = [];
+          let i = 0;
 
-                while (i < arg.length) {
-                    let object = arg[i];
-                    let signe = arg[i + 1];
-                    let valeur = arg[i + 2];
-                    let texte = [];
-                    i += 2;
+          while (i < arg.length) {
+            let object = arg[i];
+            let signe = arg[i + 1];
+            let valeur = arg[i + 2];
+            let texte = [];
+            i += 3;
 
-                    // Collecte tout le texte jusqu'à ce qu'un mot clé soit rencontré
-                    while (i < arg.length && !colonnesJoueur[arg[i]]) {
-                        texte.push(arg[i]);
-                        i++;
-                    }
-
-                    let colonneObjet = colonnesJoueur[object];
-                    let newValue;
-                    let oldValue;
-
-                    if (signe === '+' || signe === '-') {
-                        const query = `SELECT ${colonneObjet} FROM northdiv WHERE id = 14`; 
-                        const result = await client.query(query);
-                        oldValue = result.rows[0][colonneObjet];
-                        newValue = eval(`${oldValue} ${signe} ${valeur}`);
-                    } else if (signe === '=' || signe === 'add' || signe === 'supp') {
-                        const query = `SELECT ${colonneObjet} FROM northdiv WHERE id = 14`; 
-                        const result = await client.query(query);
-                        oldValue = result.rows[0][colonneObjet];
-                        if(signe === '=') {
-                            newValue = `${texte.join(' ')}`;
-                        } else if (signe === 'add') {
-                            newValue = `${oldValue} ${texte.join(' ')}`;
-                        } else if (signe === 'supp') {
-                            const regex = new RegExp(`\\b${normalizeText(texte.join(' '))}\\b`, 'gi');
-                            newValue = normalizeText(oldValue).replace(regex, '').trim();
-                        }
-                    } else {
-                        console.log("Signe invalide.");
-                        repondre('Une erreur est survenue. Veuillez entrer correctement les données.');
-                        return;
-                    }
-
-                    updates.push({ colonneObjet, newValue, oldValue, object });
-                }
-
-                try {
-                    await client.query('BEGIN');
-
-                    for (const update of updates) {
-                        const query = `UPDATE northdiv SET ${update.colonneObjet} = $1 WHERE id = 14`;
-                        await client.query(query, [update.newValue]);
-                    }
-
-                    await client.query('COMMIT');
-
-                    const messages = updates.map(update => `⚙ Object: ${update.object}\n💵 Ancienne Valeur: ${update.oldValue}\n💵 Nouvelle Valeur: ${update.newValue}`).join('\n\n');
-                    await repondre(`Données du joueur mises à jour pour:\n\n${messages}`);
-                } catch (error) {
-                    await client.query('ROLLBACK');
-                    console.error("Erreur lors de la mise à jour des données de l'utilisateur:", error);
-                    repondre('Une erreur est survenue lors de la mise à jour des données. Veuillez réessayer');
-                } finally {
-                    client.release();
-                }
-            } else {
-                repondre('Seul les Membres de la NS ont le droit de modifier cette fiche');
+            // Collecte tout le texte jusqu'à ce qu'un mot clé soit rencontré
+            while (i < arg.length && !colonnesJoueur[arg[i]] && !['=', '+', '-', 'add', 'supp'].includes(arg[i])) {
+              texte.push(arg[i]);
+              i++;
             }
-        }
-    } catch (error) {
-        console.error("Erreur lors de la mise à jour des données de l'utilisateur:", error);
-        repondre('Une erreur est survenue. Veuillez réessayer');
-    } finally {
-        if (client) {
+
+            let colonneObjet = colonnesJoueur[object];
+            let newValue;
+            let oldValue;
+
+            if (signe === '+' || signe === '-') {
+              const query = `SELECT ${colonneObjet} FROM northdiv WHERE id = 14`; 
+              const result = await client.query(query);
+              oldValue = result.rows[0][colonneObjet];
+              newValue = eval(`${oldValue} ${signe} ${valeur}`);
+            } else if (signe === '=' || signe === 'add' || signe === 'supp') {
+              const query = `SELECT ${colonneObjet} FROM northdiv WHERE id = 14`; 
+              const result = await client.query(query);
+              oldValue = result.rows[0][colonneObjet];
+              if(signe === '=') {
+                newValue = `${texte.join(' ')}`;
+              } else if (signe === 'add') {
+                newValue = `${oldValue} ${texte.join(' ')}`;
+              } else if (signe === 'supp') {
+                const regex = new RegExp(`\\b${normalizeText(texte.join(' '))}\\b`, 'gi');
+                newValue = normalizeText(oldValue).replace(regex, '').trim();
+              }
+            } else {
+              console.log("Signe invalide.");
+              repondre('Une erreur est survenue. Veuillez entrer correctement les données.');
+              return;
+            }
+
+            updates.push({ colonneObjet, newValue, oldValue, object });
+          }
+
+          try {
+            await client.query('BEGIN');
+
+            for (const update of updates) {
+              const query = `UPDATE northdiv SET ${update.colonneObjet} = $1 WHERE id = 14`;
+              await client.query(query, [update.newValue]);
+            }
+
+            await client.query('COMMIT');
+
+            const messages = updates.map(update => `⚙ Object: ${update.object}\n💵 Ancienne Valeur: ${update.oldValue}\n💵 Nouvelle Valeur: ${update.newValue}`).join('\n\n');
+            await repondre(`Données du joueur mises à jour pour:\n\n${messages}`);
+          } catch (error) {
+            await client.query('ROLLBACK');
+            console.error("Erreur lors de la mise à jour des données de l'utilisateur:", error);
+            repondre('Une erreur est survenue lors de la mise à jour des données. Veuillez réessayer');
+          } finally {
             client.release();
+          }
+        } else {
+          repondre('Seul les Membres de la NS ont le droit de modifier cette fiche');
         }
+      }
+    } catch (error) {
+      console.error("Erreur lors de la mise à jour des données de l'utilisateur:", error);
+      repondre('Une erreur est survenue. Veuillez réessayer');
+    } finally {
+      if (client) {
+        client.release();
+      }
     }
   }
 );
