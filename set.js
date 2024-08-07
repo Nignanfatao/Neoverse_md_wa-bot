b57869ff6443dd7ac5985162caacd06144285a24
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-08-07 02:30:42 +0000

    Update set.js

--- set.js
+++ set.js
@@ -7,7 +7,8 @@ module.exports = {
     PREFIXE: process.env.PREFIXE || "/",            
     MODE: process.env.MODE_PUBLIC || "oui",
     DB: process.env.DB || 'postgres://neoverse:pomrleUMXwlmlpIcW2oFJmMX0CXzaFkf@dpg-combonun7f5s73d7uoog-a.oregon-postgres.render.com/neoverse_wz98',
-                  
+    NOM_OWNER: process.env.NOM_OWNER || "Fatao",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "22651463203",             
 };
 let fichier = require.resolve(__filename);
 fs.watchFile(fichier, () => {
