e0f0821b94bcb872c5a8cfa9d9e39af089876536
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-08-07 21:22:38 +0000

    Update cards.js

--- commandes/cards.js
+++ commandes/cards.js
@@ -1,46 +1,130 @@
-module.exports = {
+/*module.exports = {
     cards: {
         sparking: [
             {
-                grade: "A",
+                grade: "or",
                 name: "Sparking Card 1",
                 image: "http://example.com/sparking1.jpg",
                 price: 20.99
             },
             {
-                grade: "B",
+                grade: "argent",
                 name: "Sparking Card 2",
+                image: "http://example.com/sparking2.jpg",
+                price: 18.99
+            }, 
+            {
+                grade: "bronze",
+                name: "Sparking Card 3",
                 image: "http://example.com/sparking2.jpg",
                 price: 18.99
             }
         ],
         ultra: [
             {
-                grade: "A",
+                grade: "or",
                 name: "Ultra Card 1",
-                image: "http://example.com/ultra1.jpg",
-                price: 30.99
+                image: "http://example.com/sparking1.jpg",
+                price: 20.99
             },
             {
-                grade: "B",
+                grade: "argent",
                 name: "Ultra Card 2",
-                image: "http://example.com/ultra2.jpg",
-                price: 25.99
+                image: "http://example.com/sparking2.jpg",
+                price: 18.99
+            }, 
+            {
+                grade: "bronze",
+                name: "Ultra Card 3",
+                image: "http://example.com/sparking2.jpg",
+                price: 18.99
             }
         ],
         legends: [
             {
-                grade: "A",
+                grade: "or",
                 name: "Legends Card 1",
-                image: "http://example.com/legends1.jpg",
-                price: 50.99
+                image: "http://example.com/sparking1.jpg",
+                price: 20.99
             },
             {
-                grade: "B",
+                grade: "argent",
                 name: "Legends Card 2",
-                image: "http://example.com/legends2.jpg",
-                price: 45.99
+                image: "http://example.com/sparking2.jpg",
+                price: 18.99
+            }, 
+            {
+                grade: "bronze",
+                name: "Legends Card 3",
+                image: "http://example.com/sparking2.jpg",
+                price: 18.99
+            }
+        ]
+    }
+};*/
+
+module.exports = {
+    cards: {
+        sparking: [
+            {
+                grade: "or",
+                name: "Sparking Card 1",
+                image: "http://example.com/sparking1.jpg",
+                price: 20.99
+            },
+            {
+                grade: "argent",
+                name: "Sparking Card 2",
+                image: "http://example.com/sparking2.jpg",
+                price: 18.99
+            }, 
+            {
+                grade: "bronze",
+                name: "Sparking Card 3",
+                image: "http://example.com/sparking2.jpg",
+                price: 18.99
+            }
+        ],
+        ultra: [
+            {
+                grade: "or",
+                name: "Ultra Card 1",
+                image: "http://example.com/sparking1.jpg",
+                price: 20.99
+            },
+            {
+                grade: "argent",
+                name: "Ultra Card 2",
+                image: "http://example.com/sparking2.jpg",
+                price: 18.99
+            }, 
+            {
+                grade: "bronze",
+                name: "Ultra Card 3",
+                image: "http://example.com/sparking2.jpg",
+                price: 18.99
+            }
+        ],
+        legends: [
+            {
+                grade: "or",
+                name: "Legends Card 1",
+                image: "http://example.com/sparking1.jpg",
+                price: 20.99
+            },
+            {
+                grade: "argent",
+                name: "Legends Card 2",
+                image: "http://example.com/sparking2.jpg",
+                price: 18.99
+            }, 
+            {
+                grade: "bronze",
+                name: "Legends Card 3",
+                image: "http://example.com/sparking2.jpg",
+                price: 18.99
             }
         ]
     }
 };
+
