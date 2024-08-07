c524d77c6054ef6f893e7114d9848049f363b607
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-08-07 01:55:59 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,35 +1,13 @@
 const fs = require('fs-extra');
 const { Sequelize } = require('sequelize');
-if (fs.existsSync('set.env'))
-    require('dotenv').config({ path: __dirname + '/set.env' });
 const path = require("path");
+
 module.exports = { 
     SESSION_ID: process.env.SESSION_ID || 'zokk',
-    PREFIXE: process.env.PREFIXE || "/",
-    NOM_OWNER: process.env.NOM_OWNER || "Fatao",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "Fatao",              
+    PREFIXE: process.env.PREFIXE || "/",            
     MODE: process.env.MODE_PUBLIC || "oui",
-    BOT : process.env.NOM_BOT || 'Zokou_MD',
-    CHAT_BOT: process.env.CHAT_BOT || 'oui', 
-    URL : process.env.LIENS_MENU || 'https://static.animecorner.me/2023/08/op2.jpg',
-    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
-    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
     DB: process.env.DB || 'postgres://neoverse:pomrleUMXwlmlpIcW2oFJmMX0CXzaFkf@dpg-combonun7f5s73d7uoog-a.oregon-postgres.render.com/neoverse_wz98',
-                  /* new Sequelize({
-     dialect: 'sqlite',
-     storage: DATABASE_URL,
-     logging: false,
-})
-: new Sequelize(DATABASE_URL, {
-     dialect: 'postgres',
-     ssl: true,
-     protocol: 'postgres',
-     dialectOptions: {
-         native: true,
-         ssl: { require: true, rejectUnauthorized: false },
-     },
-     logging: false,
-}),*/
+                  
 };
 let fichier = require.resolve(__filename);
 fs.watchFile(fichier, () => {
