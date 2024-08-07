bdad387d29f87e595d1009630016dd60264a63bb
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-08-07 20:24:46 +0000

    Create cards.js

---
+++ commandes/cards.js
@@ -0,0 +1,46 @@
+module.exports = {
+    cards: {
+        sparking: [
+            {
+                grade: "A",
+                name: "Sparking Card 1",
+                image: "http://example.com/sparking1.jpg",
+                price: 20.99
+            },
+            {
+                grade: "B",
+                name: "Sparking Card 2",
+                image: "http://example.com/sparking2.jpg",
+                price: 18.99
+            }
+        ],
+        ultra: [
+            {
+                grade: "A",
+                name: "Ultra Card 1",
+                image: "http://example.com/ultra1.jpg",
+                price: 30.99
+            },
+            {
+                grade: "B",
+                name: "Ultra Card 2",
+                image: "http://example.com/ultra2.jpg",
+                price: 25.99
+            }
+        ],
+        legends: [
+            {
+                grade: "A",
+                name: "Legends Card 1",
+                image: "http://example.com/legends1.jpg",
+                price: 50.99
+            },
+            {
+                grade: "B",
+                name: "Legends Card 2",
+                image: "http://example.com/legends2.jpg",
+                price: 45.99
+            }
+        ]
+    }
+};
