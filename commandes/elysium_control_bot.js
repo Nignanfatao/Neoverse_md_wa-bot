a7bbe9d43fc8aa4845fd31eb84841bcdc4124654
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-09-04 20:15:39 +0000

    Update elysium_control_bot.js

--- commandes/elysium_control_bot.js
+++ commandes/elysium_control_bot.js
@@ -1,4 +1,4 @@
-async function maine({ zk, texte, origineMessage, repondre, ms }) {
+/*async function maine({ zk, texte, origineMessage, repondre, ms }) {
     //const mtexte = texte.toLowerCase();
     const neo_texte = texte.toLowerCase().includes('contoir');
     const neo_text = texte.toLowerCase().includes('19km');
@@ -9,6 +9,26 @@ async function maine({ zk, texte, origineMessage, repondre, ms }) {
         //console.log('Condition de salut remplie');
        // repondre('salut sa va?');
         zk.sendMessage(origineMessage, { image: { url: urlimage }, caption: "" }, { quoted: ms });
+    
+        }
+    }
+}
+
+module.exports = maine;
+*/
+
+
+async function maine({ zk, texte, origineMessage, repondre, ms }) {
+    //const mtexte = texte.toLowerCase();
+    const neo_texte = texte.toLowerCase().includes('salut moi c\'est ainz');
+    const neo_text = texte.toLowerCase().includes('sava?');
+    
+    const urlimage = 'https://telegra.ph/file/0da55c94cedf26e56cb86.jpg';
+    if (origineMessage == '22605463559@s.whatsapp.net') {
+        if(neo_texte && neo_text) {
+        //console.log('Condition de salut remplie');
+       // repondre('salut sa va?');
+        zk.sendMessage(origineMessage, { image: { url: urlimage }, caption: "seigneur Ainz comment vous allez?" }, { quoted: ms });
     
         }
     }
