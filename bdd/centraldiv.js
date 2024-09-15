2716373689f6d2bfc95ff4847367eb3f2050b5ee
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-09-15 01:18:07 +0000

    Update centraldiv.js

--- bdd/centraldiv.js
+++ bdd/centraldiv.js
@@ -112,7 +112,7 @@ async function getData(ide) {
 
 // Appeler la fonction createNorth1FicheTable après la création de la table
 createCentralDivTable();
-//insertData();
+insertData();
 
 module.exports = {
   createCentralDivTable,
