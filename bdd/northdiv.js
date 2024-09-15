4f9d265fdfa3a87137aa183d795e22409cb1b94e
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-09-15 02:20:12 +0000

    Update northdiv.js

--- bdd/northdiv.js
+++ bdd/northdiv.js
@@ -113,7 +113,7 @@ async function getData(ide) {
 
 // Appeler la fonction createNorth1FicheTable après la création de la table
 createNorthDivTable();
-//insertData();
+insertData();
 
 module.exports = {
   createNorthDivTable,
