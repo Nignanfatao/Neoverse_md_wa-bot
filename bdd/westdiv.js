348c0f7feab9408ecc7263da6fb8a98122153576
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-09-15 01:16:53 +0000

    Update westdiv.js

--- bdd/westdiv.js
+++ bdd/westdiv.js
@@ -113,7 +113,7 @@ async function getData(ide) {
 
 // Appeler la fonction createNorth1FicheTable après la création de la table
 createWestDivTable();
-//insertData();
+insertData();
 
 module.exports = {
   createWestDivTable,
