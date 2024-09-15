86d7d832ddeab95a0adaec7fde9aca1d5b1af902
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-09-15 00:12:59 +0000

    Update cards.js

--- commandes/cards.js
+++ commandes/cards.js
@@ -1,86 +1,24 @@
-/*module.exports = {
-    cards: {
-        sparking: [
-            {
-                grade: "or",
-                name: "Sparking Card 1",
-                image: "http://example.com/sparking1.jpg",
-                price: 20.99
-            },
-            {
-                grade: "argent",
-                name: "Sparking Card 2",
-                image: "http://example.com/sparking2.jpg",
-                price: 18.99
-            }, 
-            {
-                grade: "bronze",
-                name: "Sparking Card 3",
-                image: "http://example.com/sparking2.jpg",
-                price: 18.99
-            }
-        ],
-        ultra: [
-            {
-                grade: "or",
-                name: "Ultra Card 1",
-                image: "http://example.com/sparking1.jpg",
-                price: 20.99
-            },
-            {
-                grade: "argent",
-                name: "Ultra Card 2",
-                image: "http://example.com/sparking2.jpg",
-                price: 18.99
-            }, 
-            {
-                grade: "bronze",
-                name: "Ultra Card 3",
-                image: "http://example.com/sparking2.jpg",
-                price: 18.99
-            }
-        ],
-        legends: [
-            {
-                grade: "or",
-                name: "Legends Card 1",
-                image: "http://example.com/sparking1.jpg",
-                price: 20.99
-            },
-            {
-                grade: "argent",
-                name: "Legends Card 2",
-                image: "http://example.com/sparking2.jpg",
-                price: 18.99
-            }, 
-            {
-                grade: "bronze",
-                name: "Legends Card 3",
-                image: "http://example.com/sparking2.jpg",
-                price: 18.99
-            }
-        ]
-    }
-};*/
-
 module.exports = {
     cards: {
         sparking: [
             {
                 grade: "or",
                 name: "Sparking Card 1",
+                category: "s-",
                 image: "http://example.com/sparking1.jpg",
                 price: 20.99
             },
             {
                 grade: "argent",
                 name: "Sparking Card 2",
+                category: "s-",
                 image: "http://example.com/sparking2.jpg",
                 price: 18.99
             }, 
             {
                 grade: "bronze",
                 name: "Sparking Card 3",
+                category: "s-",
                 image: "http://example.com/sparking2.jpg",
                 price: 18.99
             }
@@ -89,18 +27,21 @@ module.exports = {
             {
                 grade: "or",
                 name: "Ultra Card 1",
+                category: "s-",
                 image: "http://example.com/sparking1.jpg",
                 price: 20.99
             },
             {
                 grade: "argent",
                 name: "Ultra Card 2",
+                category: "s-",
                 image: "http://example.com/sparking2.jpg",
                 price: 18.99
             }, 
             {
                 grade: "bronze",
                 name: "Ultra Card 3",
+                category: "s-",
                 image: "http://example.com/sparking2.jpg",
                 price: 18.99
             }
@@ -109,22 +50,24 @@ module.exports = {
             {
                 grade: "or",
                 name: "Legends Card 1",
+                category: "s-",
                 image: "http://example.com/sparking1.jpg",
                 price: 20.99
             },
             {
                 grade: "argent",
                 name: "Legends Card 2",
+                category: "s-",
                 image: "http://example.com/sparking2.jpg",
                 price: 18.99
             }, 
             {
                 grade: "bronze",
                 name: "Legends Card 3",
+                category: "s-",
                 image: "http://example.com/sparking2.jpg",
                 price: 18.99
             }
         ]
     }
 };
-
