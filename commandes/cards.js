a7df95bb887c2750ac88311ffd5ce6484bdb492c
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-09-16 18:11:45 +0000

    Update cards.js

--- commandes/cards.js
+++ commandes/cards.js
@@ -6,217 +6,217 @@ module.exports = {
                 grade: "bronze",
                 name: "Asta",
                 category: "s",
-                image: "../Card_data/Asta_bronze_sparking_s_300k.jpg",
+                image: "./Card_data/Asta_bronze_sparking_s_300k.jpg",
                 price: "300.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Choso",
                 category: "s",
-                image: "../Card_data/Choso_bronze_sparking_s_300k.jpg",
+                image: "./Card_data/Choso_bronze_sparking_s_300k.jpg",
                 price: "300.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Dagon",
                 category: "s",
-                image: "../Card_data/Dagon_bronze_sparking_s_300k.jpg",
+                image: "./Card_data/Dagon_bronze_sparking_s_300k.jpg",
                 price: "300.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Gauche",
                 category: "s-",
-                image: "../Card_data/Gauche_bronze_sparking_sm_100k.jpg",
+                image: "./Card_data/Gauche_bronze_sparking_sm_100k.jpg",
                 price: "100.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Hanami",
                 category: "s+",
-                image: "../Card_data/Hanami_bronze_sparking_sp_400k.jpg",
+                image: "./Card_data/Hanami_bronze_sparking_sp_400k.jpg",
                 price: "400.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Haruta",
                 category: "s-",
-                image: "../Card_data/Haruta_bronze_sparking_sm_100k.jpg",
+                image: "./Card_data/Haruta_bronze_sparking_sm_100k.jpg",
                 price: "100.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Higurama",
                 category: "s-",
-                image: "../Card_data/Higuruma_bronze_sparking_sm_150k.jpg",
+                image: "./Card_data/Higuruma_bronze_sparking_sm_150k.jpg",
                 price: "150.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Ino",
                 category: "s-",
-                image: "../Card_data/Ino_bronze_sparking_sm_100k.jpg",
+                image: "./Card_data/Ino_bronze_sparking_sm_100k.jpg",
                 price: "100.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Inosuke",
                 category: "s-",
-                image: "../Card_data/Inosuke_bronze_sparking_sm_100k.jpg",
+                image: "./Card_data/Inosuke_bronze_sparking_sm_100k.jpg",
                 price: "100.000🧭"
             },
             {
                 grade: "argent",
                 name: "Jogo",
                 category: "s-",
-                image: "../Card_data/Jogo_argent_sparking_sm_500k.jpg",
+                image: "./Card_data/Jogo_argent_sparking_sm_500k.jpg",
                 price: "500.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Kamo",
                 category: "s-",
-                image: "../Card_data/Kamo_bronze_sparking_sm_100k.jpg",
+                image: "./Card_data/Kamo_bronze_sparking_sm_100k.jpg",
                 price: "100.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Kusakabe",
                 category: "s-",
-                image: "../Card_data/Kusakabe_bronze_sparking_sm_100k.jpg",
+                image: "./Card_data/Kusakabe_bronze_sparking_sm_100k.jpg",
                 price: "100.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Larue",
                 category: "s-",
-                image: "../Card_data/Larue_bronze_sparking_sm_100k.jpg",
+                image: "./Card_data/Larue_bronze_sparking_sm_100k.jpg",
                 price: "100.000🧭"
             },
             {
                 grade: "argent",
                 name: "Maki",
                 category: "s-",
-                image: "../Card_data/Maki_argent_sparking_sm_500k.jpg",
+                image: "./Card_data/Maki_argent_sparking_sm_500k.jpg",
                 price: "500.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Megumi",
                 category: "s-",
-                image: "../Card_data/Megumi_bronze_sparking_sm_100k.jpg",
+                image: "./Card_data/Megumi_bronze_sparking_sm_100k.jpg",
                 price: "100.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Mechamaru",
                 category: "s-",
-                image: "../Card_data/Mechamaru_bronze_sparking_sm_150k.jpg",
+                image: "./Card_data/Mechamaru_bronze_sparking_sm_150k.jpg",
                 price: "150.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Miguel",
                 category: "s-",
-                image: "../Card_data/Miguel_bronze_sparking_sm_100k.jpg",
+                image: "./Card_data/Miguel_bronze_sparking_sm_100k.jpg",
                 price: "100.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Nanami",
                 category: "s-",
-                image: "../Card_data/Nanami_bronze_sparking_sm_150k.jpg",
+                image: "./Card_data/Nanami_bronze_sparking_sm_150k.jpg",
                 price: "150.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Naobito",
                 category: "s",
-                image: "../Card_data/Naobito_bronze_sparking_s_250k.jpg",
+                image: "./Card_data/Naobito_bronze_sparking_s_250k.jpg",
                 price: "250.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Naoya",
                 category: "s",
-                image: "../Card_data/Naoya_bronze_sparking_s_300k.jpg",
+                image: "./Card_data/Naoya_bronze_sparking_s_300k.jpg",
                 price: "300.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Nezuko",
                 category: "s-",
-                image: "../Card_data/Nezuko_bronze_sparking_sm_100k.jpg",
+                image: "./Card_data/Nezuko_bronze_sparking_sm_100k.jpg",
                 price: "100.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Panda",
                 category: "s-",
-                image: "../Card_data/Panda_bronze_sparking_sm_100k.jpg",
+                image: "./Card_data/Panda_bronze_sparking_sm_100k.jpg",
                 price: "100.000🧭"
             },
             {
                 grade: "argent",
                 name: "Ryu",
                 category: "s-",
-                image: "../Card_data/Ryu_argent_sparking_sm_500k.jpg",
+                image: "./Card_data/Ryu_argent_sparking_sm_500k.jpg",
                 price: "500.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Tanjiro",
                 category: "s-",
-                image: "../Card_data/Tanjiro_bronze_sparking_sm_100k.jpg",
+                image: "./Card_data/Tanjiro_bronze_sparking_sm_100k.jpg",
                 price: "100.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Tatako",
                 category: "s-",
-                image: "../Card_data/Tatako_argent_sparking_sm_500k.jpg",
+                image: "./Card_data/Tatako_argent_sparking_sm_500k.jpg",
                 price: "500.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Toge",
                 category: "s-",
-                image: "../Card_data/Toge_bronze_sparking_sm_100k.jpg",
+                image: "./Card_data/Toge_bronze_sparking_sm_100k.jpg",
                 price: "100.000🧭"
             },
             {
                 grade: "argent",
                 name: "Yorozu",
                 category: "s-",
-                image: "../Card_data/Yorozu_argent_sparking_sm_500k.jpg",
+                image: "./Card_data/Yorozu_argent_sparking_sm_500k.jpg",
                 price: "500.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Yuji(shibuya-Incident)",
                 category: "s",
-                image: "../Card_data/YujiShibuyaIncident_bronze_sparking_s_200k.jpg",
+                image: "./Card_data/YujiShibuyaIncident_bronze_sparking_s_200k.jpg",
                 price: "200.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Yuji",
                 category: "s-",
-                image: "../Card_data/Yuji_bronze_sparking_sm_100k.jpg",
+                image: "./Card_data/Yuji_bronze_sparking_sm_100k.jpg",
                 price: "100.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Zenitsu",
                 category: "s-",
-                image: "../Card_data/Zenitsu_bronze_sparking_sm_200k.jpg",
+                image: "./Card_data/Zenitsu_bronze_sparking_sm_200k.jpg",
                 price: "200.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Zora",
                 category: "s-",
-                image: "../Card_data/Zora_bronze_sparking_sm_100k.jpg",
+                image: "./Card_data/Zora_bronze_sparking_sm_100k.jpg",
                 price: "100.000🧭"
             }
         ],
@@ -227,98 +227,98 @@ module.exports = {
                 grade: "bronze",
                 name: "Aoi",
                 category: "s",
-                image: "../Card_data/Aoi_bronze_ultra_s_350k.jpg",
+                image: "./Card_data/Aoi_bronze_ultra_s_350k.jpg",
                 price: "350.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Asta",
                 category: "s+",
-                image: "../Card_data/Asta_bronze_ultra_sp_450k.jpg",
+                image: "./Card_data/Asta_bronze_ultra_sp_450k.jpg",
                 price: "450.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Geto(jujutsu kayen 0)",
                 category: "s+",
-                image: "../Card_data/GetoJjk0_bronze_ultra_sp_400k.jpg",
+                image: "./Card_data/GetoJjk0_bronze_ultra_sp_400k.jpg",
                 price: "400.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Geto",
                 category: "s",
-                image: "../Card_data/Geto_bronze_ultra_s_350k.jpg",
+                image: "./Card_data/Geto_bronze_ultra_s_350k.jpg",
                 price: "350.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Giyu",
                 category: "s",
-                image: "../Card_data/Giyu_bronze_ultra_s_300k.jpg",
+                image: "./Card_data/Giyu_bronze_ultra_s_300k.jpg",
                 price: "300.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Hakari",
                 category: "s+",
-                image: "../Card_data/Hakari_bronze_ultra_sp_400k.jpg",
+                image: "./Card_data/Hakari_bronze_ultra_sp_400k.jpg",
                 price: "400.000🧭"
             },
             {
                 grade: "argent",
                 name: "Kashimo",
                 category: "s-",
-                image: "../Card_data/Kashimo_argent_ultra_sm_500k.jpg",
+                image: "./Card_data/Kashimo_argent_ultra_sm_500k.jpg",
                 price: "500.000🧭"
             },
             {
                 grade: "argent",
                 name: "Kenjaku",
                 category: "s-",
-                image: "../Card_data/Kenjaku_argent_ultra_sm_500k.jpg",
+                image: "./Card_data/Kenjaku_argent_ultra_sm_500k.jpg",
                 price: "500.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Mahito",
                 category: "s-",
-                image: "../Card_data/Mahito_bronze_ultra_sm_350k.jpg",
+                image: "./Card_data/Mahito_bronze_ultra_sm_350k.jpg",
                 price: "350.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Mahito",
                 category: "s+",
-                image: "Mahito_bronze_ultra_sp_400k.jpg",
+                image: "./Card_data/Mahito_bronze_ultra_sp_400k.jpg",
                 price: "400.000🧭"
             },
             {
                 grade: "bronze",
                 name: "Maki",
                 category: "s",
-                image: "Maki_bronze_ultra_s_300k.jpg",
+                image: "./Card_data/Maki_bronze_ultra_s_300k.jpg",
                 price: "300.000🧭"
             },
             {
                 grade: "argent",
                 name: "Toji",
                 category: "s-",
-                image: "Toji_argent_ultra_sm_600k.jpg",
+                image: "./Card_data/Toji_argent_ultra_sm_600k.jpg",
                 price: "600.000🧭"
             },
             {
                 grade: "argent",
                 name: "Uraume",
                 category: "s-",
-                image: "../Card_data/Uraume_argent_ultra_sm_500k.jpg",
+                image: "./Card_data/Uraume_argent_ultra_sm_500k.jpg",
                 price: "500.000🧭"
             },
             {
                 grade: "argent",
                 name: "Yuta",
                 category: "s-",
-                image: "../Card_data/Yuta_argent_ultra_sm_50nc.jpg",
+                image: "./Card_data/Yuta_argent_ultra_sm_50nc.jpg",
                 price: "50🔷"
             }
         ],
@@ -329,35 +329,35 @@ module.exports = {
                 grade: "argent",
                 name: "Gojo",
                 category: "s",
-                image: "../Card_data/Gojo_argent_legend_s_800k.jpg",
+                image: "./Card_data/Gojo_argent_legend_s_800k.jpg",
                 price: "800.000🧭"
             },
             {
                 grade: "argent",
                 name: "Gojo",
                 category: "s+",
-                image: "../Card_data/Gojo_argent_legend_sp_100nc.jpg",
+                image: "./Card_data/Gojo_argent_legend_sp_100nc.jpg",
                 price: "100🔷"
             }, 
             {
                 grade: "bronze",
                 name: "Gojo",
                 category: "s+",
-                image: "../Card_data/Gojo_bronze_legend_sp_50nc.jpg",
+                image: "./Card_data/Gojo_bronze_legend_sp_50nc.jpg",
                 price: "50🔷"
             },
             {
                 grade: "bronze",
                 name: "Sukuna(4 Fingers)",
                 category: "s+",
-                image: "../Card_data/Sukuna4Fingers_bronze_legend_sp_50nc.jpg",
+                image: "./Card_data/Sukuna4Fingers_bronze_legend_sp_50nc.jpg",
                 price: "50🔷"
             },
             {
                 grade: "bronze",
                 name: "Yuta (jujutsu kaisen 0)",
                 category: "s+",
-                image: "../Card_data/YutaJjk0_bronze_legend_sp_400k.jpg",
+                image: "./Card_data/YutaJjk0_bronze_legend_sp_400k.jpg",
                 price: "400.000🧭"
             }
            /* {
