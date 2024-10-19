f0937a0b7e59e15ee50c5abcaea5f1a31a951ae0
Ainz <140420451+Nignanfatao@users.noreply.github.com>
2024-10-19 15:15:56 +0000

    Update northdiv.js

--- Id_ext/northdiv.js
+++ Id_ext/northdiv.js
@@ -1,5 +1,6 @@
 const ainz = [
-  { "id": "22605463559@s.whatsapp.net" }
+  { id: "22605463559@s.whatsapp.net", nom: "Ainz" },
+  { id: "22651463203@s.whatsapp.net", nom: "Ainz-bot" }
 ];
 
 module.exports = ainz;
