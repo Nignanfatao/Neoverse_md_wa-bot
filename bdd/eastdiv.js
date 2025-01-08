7d3e20055348a9887612f8d6acccce2fdc599800
Ainz <140420451+Nignanfatao@users.noreply.github.com>
2025-01-08 22:01:51 +0000

    Update eastdiv.js

--- bdd/eastdiv.js
+++ bdd/eastdiv.js
@@ -42,7 +42,12 @@ async function createEastDivTable() {
         e20 INTEGER DEFAULT 0,
         e21 INTEGER DEFAULT 0,
         e22 INTEGER DEFAULT 0,
-        e23 TEXT DEFAULT 'aucun'
+        e23 TEXT DEFAULT 'aucun',
+        e24 INTEGER DEFAULT 0,
+        e25 INTEGER DEFAULT 0,
+        e26 INTEGER DEFAULT 0,
+        e27 INTEGER DEFAULT 0,
+        e28 INTEGER DEFAULT 0
         );
     `);
     console.log('Table eastdiv créée avec succès');
@@ -60,7 +65,12 @@ async function insertData1() {
     // Modifiez la définition de la table pour ajouter les colonnes
     await client.query(`
       ALTER TABLE eastdiv
-      ADD COLUMN e23 TEXT DEFAULT 'aucun'
+      ADD COLUMN e24 INTEGER DEFAULT 0,
+      ADD COLUMN e25 INTEGER DEFAULT 0,
+      ADD COLUMN e26 INTEGER DEFAULT 0,
+      ADD COLUMN e27 INTEGER DEFAULT 0,
+      ADD COLUMN e28 INTEGER DEFAULT 0
+     
      `);
 
     console.log('Colonnes ajoutées avec succès');
@@ -70,19 +80,19 @@ async function insertData1() {
     client.release();
   }
 }
-//insertData1()
+insertData1()
 // Fonction pour insérer des données
 async function insertData() {
   const client = await pool.connect();
   try {
     for (let i = 1; i <= 50; i++) {
       const query = `
-        INSERT INTO eastdiv(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16, e17, e18, e19, e20, e21, e22, e23)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23)
+        INSERT INTO eastdiv(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16, e17, e18, e19, e20, e21, e22, e23, e24, e25, e26, e27, e28)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28)
       `;
 
       const values = [
-        'aucun', 'aucun', 'aucun', 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 0, 0, 0, 0, 0, 'aucun'
+        'aucun', 'aucun', 'aucun', 'aucun', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'aucun', 0, 0, 0, 0, 0, 'aucun', 0, 0, 0, 0, 0
       ];
 
       await client.query(query, values);
@@ -99,7 +109,7 @@ async function getData(ide) {
   const client = await pool.connect();
 
   try {
-   const query = 'SELECT e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16, e17, e18, e19, e20, e21, e22, e23 FROM eastdiv WHERE id = $1';
+   const query = 'SELECT e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16, e17, e18, e19, e20, e21, e22, e23, e24, e25, e26, e27, e28 FROM eastdiv WHERE id = $1';
     const result = await client.query(query, [ide]);
 
     return result.rows[0];
@@ -113,7 +123,7 @@ async function getData(ide) {
 
 // Appeler la fonction createNorth1FicheTable après la création de la table
 createEastDivTable();
-//insertData();
+insertData();
 
 module.exports = {
   createEastDivTable,
