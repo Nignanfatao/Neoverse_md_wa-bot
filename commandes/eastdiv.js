2e1ad5c93a538fd118cad15700f23e4aeeb0d19e
Ainz <140420451+Nignanfatao@users.noreply.github.com>
2025-01-09 22:41:52 +0000

    Update eastdiv.js

--- commandes/eastdiv.js
+++ commandes/eastdiv.js
@@ -24,7 +24,7 @@ function add_fiche(nom_joueur, data_id, image_oc) {
 ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
 ◇ *Pseudo👤*: ${data.e1}
 ◇ *Position Continentale🌍:* ${data.e23}
-◇ *Division🛡️*: *${data.e2}
+◇ *Division🛡️*: ${data.e2}
 ◇ *Rang 🎖️*: ${data.e4}
 ◇ *Classe🏆*: ${data.e3}
 ▔▔▔▔▔▔▔▔▔▔▔░▒▒▒▒░░▒░
