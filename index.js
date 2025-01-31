85f9f463d89ad6724800bdd8a8159688907274ca
Ainz <140420451+Nignanfatao@users.noreply.github.com>
2025-01-31 18:10:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -266,6 +266,49 @@ async function main() {
             }
         });
 
+        zk.ev.on('group-participants.update', async (data) => {
+    const parseID = (jid) => {
+        if (!jid) return jid;
+        if (/:\d+@/gi.test(jid)) {
+            const decode = jidDecode(jid) || {};
+            return (decode.user && decode.server && `${decode.user}@${decode.server}`) || jid;
+        }
+        return jid;
+    };
+
+    const groupPic = 'https://files.catbox.moe/ehnubw.jpg';
+    try {
+         if (data.action === 'add' && data.id == '120363031940789145@g.us' && conf.WELCOME == 'oui') {
+            const newMembers = data.participants.map(m => `@${m.split("@")[0]}`).join('\n');
+            const mek = `🎉 🔷 *🎉WELCOME 𝗮̀ 🔷𝗡Ξ𝗢𝘃𝗲𝗿𝘀𝗲🎉* 🎮
+░▒▒▒▒░░▒░▔▔▔▔▔▔▔▔▔▔▔▔▔
+Bienvenue à vous *${newMembers}* 😃💙👋🏻, ceci est le salon de Recrutement des nouveaux joueurs ! Une fois avoir lu et terminé les conditions d'intégration, vous serez ajoutés dans le Salon principal. #NEONation💙 #Welcome💙👋🏻🙂. 
+
+🔷🎮 *𝖢𝖮𝖭𝖣𝖨𝖳𝖨𝖮𝖭𝖲 𝖭𝖤𝖮𝗏𝖾𝗋𝗌𝖾*
+░▒▒▒▒░░▒░▔▔▔▔▔▔▔▔▔▔▔▔▔
+❓Voici comment s'enregistrer à NEOverse👇🏼:
+
+👉🏽 *ÉTAPE 1️⃣*: Votre Pseudo (Nom de joueur + Pays + Numéro de téléphone)
+👉🏽 *ÉTAPE 2️⃣:* Envoyer une photo de profil de votre avatar (de préférence un perso anime comme Blue Lock, etc.). 
+👉🏽 *ÉTAPE 3️⃣* : Follow les deux chaînes ci-dessous 
+👉🏽 *ÉTAPE 4️⃣*: Attendez votre première carte de jeu avant de demander l'intégration : https://chat.whatsapp.com/LrKSRoxMcPi133sCtQB8Hf. 
+
+*🌍NOS LIENS*👇👇👇
+▔▔▔▔▔▔▔▔▔▔▔▔▔
+👉🏽🪀 *Chaîne* : /whatsapp.com/channel/0029VaN9Z2yL2AU55DSahC23
+
+👉🏽 *🛍️RP Store* : /whatsapp.com/channel/0029VaS9ngkFHWqAHps0BL3f
+
+░░░░░░░░░░░░░░░░░░░
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+💙𝗡Ξ𝗢🙂🏆🎉`;
+            await ovl.sendMessage(data.id, { image: { url: groupPic }, caption: mek, mentions: data.participants });
+        }
+    } catch (error) {
+        console.error("Erreur lors de la gestion des participants :", error);
+    }
+        });
+        
         // Gestion des mises à jour des identifiants
         zk.ev.on("creds.update", saveCreds);
 
