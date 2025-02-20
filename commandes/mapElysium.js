45a0350cbf8a8aff4f5eaf75d4626d1220c9f09e
WhiteKINGS-Th <anthonycurtis403@gmail.com>
2025-02-20 09:05:57 +0000

    Create mapElysium.js

---
+++ commandes/mapElysium.js
@@ -0,0 +1,113 @@
+const { zokou } = require("../framework/zokou");
+
+const groupeFa_ID = "120363027511214270@g.us";
+
+zokou(
+    {
+        nomCom: "map_fa",
+        reaction: '🗺️",
+        categorie: "MAPS_ELYSIUM💠"
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, ms } = commandeOptions;
+
+        try {
+            
+            if (dest !== groupeFa_ID) {
+                return repondre("⚠️Cette carte n'est pas disponible dans ce zone.");
+            }
+
+            const message = `*\`♻️FALLEN ANGELES CITY🦩🎡\`*
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+🌃DISTRICT: *🏢𝗠𝗘𝗧𝗥𝗢𝗣𝗢𝗟𝗜𝗧𝗔𝗡𝗜𝗔🏙️* ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔■■■■■
+🚸 *48𝗸𝗺*: Av. Kings \`[Museum🗿]\`
+🚸 *47𝗸𝗺*: Av. Federal \`[NC Bank🏦]\` 
+🚸 *46𝗸𝗺*: Av. Collins \`[Parking🚘]\` 
+🚸 *45𝗸𝗺*: Av. Telsa \`[Metro🚄]\` 
+🚸 *44𝗸𝗺*: Av. Fain \`[GrandHôtel🏨]\`
+🚸 *43𝗸𝗺*: Av. SAT \`[Blue fish🐠🍽️]\`
+🚸 *42𝗸𝗺*: Av. Fortuna \`[JacobsCo🔑]\`
+🚸 *41𝗸𝗺*: Av. Temple \`[Business&🏢]\`
+      ══🔽 \`𝗠𝗘𝗧𝗥𝗢𝗧𝗢𝗪𝗡\`🏙️[N]═══
+◾ *40𝗸𝗺*:  Av. Rocknorth 🚔
+🚸 *39𝗸𝗺*: Av. Freetown \`[Hôpital🏨]\`
+🚸 *38𝗸𝗺*: Av. Swan \`[Memorata👛🛍️]\`
+♻️ *37𝗸𝗺*: Av. Sparks \`[Apparts🏠]\`
+🚸 *36𝗸𝗺*: Av. Eternity \`[Store🛒]\` 
+🚸 *35𝗸𝗺*: Av. Sunning \`[Neo Tech🩻]\`
+🚸 *34𝗸𝗺*: Av. liberty \`[LCD cars🚘]\` 
+🚸 *33𝗸𝗺*:Av. Métro \`[Station🚅🌍]\`
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+🛜 *Environment*:
+\`[voir description]\` 
+🛣️🚘 *Traffic intense*:
+\`[voir description]\`
+🚔: Point de contrôle de Police
+
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+🌃DISTRICT:*🍸𝗔𝗡𝗚𝗘𝗟𝗦 𝗩𝗜𝗖E⭐* [E] 
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔■■■■■
+◼️ *32𝗸𝗺*: Av. Montana \`[Parking🚘]\`
+◼️ *31𝗸𝗺*: Av. Arktown 🚔
+🚸 *30𝗸𝗺*: Av. Sharp \`[Bank ATM🏪]\` 
+🚸 *29𝗸𝗺*: Av. Eywood \`[StarGym🏋🏽‍♂️]\`
+🚸 *28𝗸𝗺*: Av. Véronica \`[Carshop🛠️]\` 
+🚸 *27𝗸𝗺*: Av. Loksfort \`[EFood🍕]\`
+🚸 *26𝗸𝗺*: Av. Nevada \`[Casino🎰]\`
+🚸 *25𝗸𝗺*: Av. Fairy \`[EVOstyle👕🛍️]\`
+         ══ 🔽 \`𝗩𝗘𝗥𝗢𝗡𝗜𝗖𝗔\`🍸[O]══
+🚸 *24𝗸𝗺*: Av. Dixies \`[Store🛒]\`
+🚸 *23𝗸𝗺*: Av. Staples \`[Cinema🍿🎞️]\`
+◼️ *22𝗸𝗺*: Av. Liberty Town
+🚸 *21𝗸𝗺*: Av. Lux \`[ClubVenus🪩🍸]\`
+🚸 *20𝗸𝗺*: Av.Freeland \`[GameHome🕹️]\`
+🚸 *19𝗸𝗺*: Av. Shine \`[MGStage🎤🪩]\`
+🚸 *18𝗸𝗺*: Av. Red miles \`[Metro🚄]\`
+♻️ *17𝗸𝗺*: Av. Xnes \`[Apparts 🏠]\`
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+🛜 *Environment*:
+\`[voir description]\` 
+🛣️🚘 *Traffic moyen*:
+\`[voir description]\` 
+🚔: Point de contrôle de Police
+
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+ 🌃DISTRICT: *🎡𝗠𝗔𝗥𝗜𝗡𝗔🦩🌴* [S] 
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔■■■■■
+🚸 *16𝗸𝗺*: Av. Tropica \`[Store🛒]\`
+🚸 *15𝗸𝗺*: Av. Santana \`[Stadium🏟️]\`
+◼️ *14𝗸𝗺*: Av. Blue Bay \`[Parking🚘]\`
+◼️ *13𝗸𝗺*: Av. Santa Veronica🦩
+🚸 *12𝗸𝗺*: Av. Seas \`[Playground🏀]\`
+🚸 *11𝗸𝗺*: Av. Haleywood \`[Metro🚄]\`
+🚸 *10𝗸𝗺*: Av. Miles \`[Joytown🎢🎠]\`
+◼️ *9𝗸𝗺*: Av. Los Flores🦩
+      ══🔽 \`𝗠𝗔𝗥𝗜𝗡𝗔 𝗕𝗘𝗔𝗖𝗛\`🦩🌴══
+🚸 *8𝗸𝗺*: Av.Reds \`[Paradise👠🔞]\` 
+🚸 *7𝗸𝗺*: Av.Maryland \`[Races🏁]\`
+◼️ *6𝗸𝗺*: Av. Westshore sea
+♻️ *5𝗸𝗺*: Av. Seattle \`[Apparts🏠]\`
+🚸 *4𝗸𝗺*: Av. Lust  \`[Red Club🔞]\`💋
+*🚸3𝗸𝗺*: Av. Playa🌴 \`[Havanah🍹]\`
+*◼️ 2𝗸𝗺*: La Marina🌴 \`[TheBay🏖️]\`
+*◼️ 1𝗸𝗺*: Long Beach🌴 \`[Plage🚤]\` 
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+🛜⚠️GANG:
+\`Tout marina beach est Le territoire du gang des EXOTICS💋\`
+🛜 *Environment*: 
+\`[voir description]\` 
+🛣️🚘 *Traffic moyen*:
+ \`[voir description]\`
+🚔: Point de contrôle de Police
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+                       ◻◻◻◻◻▢▢▢`;
+
+            const imageURL = "lien de l'image Ainz.jpg";
+
+            await zk.sendMessage(dest, { image: { url: imageURL }, caption: message }, { quoted: ms });
+        } catch (error) {
+            console.error("Erreur lors de l'envoi de la carte:", error);
+            repondre("❌Une erreur est survenue lors de l'envoi de la carte.");
+        }
+    }
+);
