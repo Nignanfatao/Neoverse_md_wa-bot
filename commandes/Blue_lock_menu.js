f82192505f3064f3173ef0d5008cf06c9478635d
Ainz <140420451+Nignanfatao@users.noreply.github.com>
2025-03-15 18:02:19 +0000

    Update Blue_lock_menu.js

--- commandes/Blue_lock_menu.js
+++ commandes/Blue_lock_menu.js
@@ -27,9 +27,7 @@ zokou(
             ];
 
             try {
-                let imageMessage = await zk.sendMessage(dest, {
-                     text: frames[0]
-                }, { quoted: ms });
+                let imageMessage = await repondre(frames[0]);
 
                 for (let i = 1; i < frames.length; i++) {
                     await new Promise(resolve => setTimeout(resolve, 50));
