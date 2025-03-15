e753a60a922cd56045477c6a712fae609c71ca39
Ainz <140420451+Nignanfatao@users.noreply.github.com>
2025-03-15 17:19:55 +0000

    Update Blue_lock_menu.js

--- commandes/Blue_lock_menu.js
+++ commandes/Blue_lock_menu.js
@@ -2,16 +2,87 @@ const { zokou } = require('../framework/zokou');
 
 zokou(
     {
-        nomCom: '',
+        nomCom: 'bluelock🔷',
         categorie: 'Other'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
 
-        if (!arg || arg.length === 0)  {
-            const lien = 'https://files.catbox.moe/06hjut.jpg';
-            const msg = '';
-            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+        if (!arg || arg.length === 0) {
+            const lienImage = 'https://files.catbox.moe/ol0i4m.jpg';
+            const lienGif = 'https://files.catbox.moe/0ua28c.mp4';
+
+            const frames = [
+                "▱▱▱▱▱▱▱▱▱▱ 🔷0%",
+                "▰▱▱▱▱▱▱▱▱▱ 🔷10%",
+                "▰▰▱▱▱▱▱▱▱▱ 🔷20%",
+                "▰▰▰▱▱▱▱▱▱▱ 🔷30%",
+                "▰▰▰▰▱▱▱▱▱▱ 🔷40%",
+                "▰▰▰▰▰▱▱▱▱▱ 🔷50%",
+                "▰▰▰▰▰▰▱▱▱▱ 🔷60%",
+                "▰▰▰▰▰▰▰▱▱▱ 🔷70%",
+                "▰▰▰▰▰▰▰▰▱▱ 🔷80%",
+                "▰▰▰▰▰▰▰▰▰▱ 🔷90%",
+                "▰▰▰▰▰▰▰▰▰▰ 🔷100%",
+            ];
+
+            try {
+                let imageMessage = await zk.sendMessage(dest, {
+                    image: { url: lienImage },
+                    caption: frames[0]
+                }, { quoted: ms });
+
+                for (let i = 1; i < frames.length; i++) {
+                    await new Promise(resolve => setTimeout(resolve, 50));
+                    await zk.sendMessage(dest, {
+                        text: frames[i],
+                        edit: imageMessage.key,
+                    });
+                }
+
+                await zk.sendMessage(dest, {
+                    video: { url: lienGif },
+                    gifPlayback: true,
+                    caption: ""
+                });
+
+                await zk.sendMessage(dest, {
+                    image: { url: lienImage },
+                    caption: ""
+                });
+
+            } catch (error) {
+                console.error("Erreur lors de l'animation :", error);
+                await zk.sendMessage(dest, { text: "Une erreur s'est produite. 😢" });
+            }
+        }
+    }
+);
+
+
+zokou(
+    {
+        nomCom: 'bluegame🔷',
+        categorie: 'Other'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0) {
+            const liens = [
+                'https://files.catbox.moe/1nvz4f.jpg', 
+                'https://files.catbox.moe/qihh25.jpg',
+                'https://files.catbox.moe/55skbu.jpg', 
+                'https://files.catbox.moe/69eev8.jpg', 
+                'https://files.catbox.moe/6ckd48.jpg', 
+                'https://files.catbox.moe/qnjv5p.jpg', 
+                'https://files.catbox.moe/y90lsj.jpg'
+            ];
+            
+            for (const lien of liens) {
+                await zk.sendMessage(dest, { image: { url: lien }, caption: "" }, { quoted: ms });
+                await new Promise(resolve => setTimeout(resolve, 100));
+            }
         }
     }
 );
