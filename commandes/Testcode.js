405333e7b0228a7fe08d7743152d0ff081030db7
WhiteKINGS-Th <anthonycurtis403@gmail.com>
2025-03-21 19:44:29 +0000

    Delete commandes/Testcode.js

--- commandes/Testcode.js
+++
@@ -1,6 +0,0 @@
-const { zokou } = require('../framework/zokou');
-
-zokou({ nomCom: 'test', categorie: 'Other' }, (dest, zk, commandeOptions) => {
-  const { repondre } = commandeOptions;
-  repondre('+eastwhite👤 golds - 500000');
-});
